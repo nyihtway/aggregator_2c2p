85ce50167eb4ec5dda72339e916a65ce8b5d11be
Nyi Htway Tin <nyihtway@gmail.com>
2021-01-26 03:48:29 +0800

    no message

--- 2C2P.DEMO.Aggregator/Models/TransactionEvent.cs
+++ 2C2P.DEMO.Aggregator/Models/TransactionEvent.cs
@@ -7,18 +7,12 @@ using System;
 
 namespace _2C2P.DEMO.Aggregator.Models
 {
-    public class TransactionEvent : IntegrationEventBase, INotification, IMapTo<Transaction>
+    public class TransactionEvent : IntegrationEventBase, INotification
     {
         public string TransactionId { get; set; }
         public decimal Amount { get; set; }
         public string CurrencyCode { get; set; }
         public DateTime TransactionDate { get; set; }
         public string Status { get; set; }
-
-        public void Mapping(Profile profile)
-        {
-            profile.CreateMap<TransactionEvent, Transaction>()
-                .ForAllMembers(opt => opt.Condition((source, dest, sourceMember, destMember) => (sourceMember != null)));
-        }
     }
 }
